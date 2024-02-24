b66725c8d4e76246cf95e806f2ef2c677f1c10a7
Saif Ali <saifali27906@gmail.com>
2024-02-24 15:08:27 +0530

    get user with all functionality done

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -25,4 +25,42 @@ const createTask = async (req, res, next) => {
   }
 };
 
-module.exports = { createTask };
+const getAllTasks = async (req, res) => {
+  const { search, status, sort, scope } = req.query;
+  const queryObject = {};
+  if (scope && scope !== "all") {
+    queryObject.createdBy = scope;
+  }
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+  if (search) {
+    queryObject.name = { $regex: search, $options: "i" };
+  }
+  let result = Task.find(queryObject);
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const tasks = await result;
+  const totalTasks = await Task.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalTasks / limit);
+  res.status(200).json({ tasks, totalTasks, numOfPages });
+};
+
+module.exports = { createTask, getAllTasks };
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask } = require("../controllers/taskController.js");
+const { createTask, getAllTasks } = require("../controllers/taskController.js");
 const auth = require("../middleware/auth.js");
 
+router.route("/").get(getAllTasks);
 router.route("/").post(auth, createTask);
 module.exports = router;
